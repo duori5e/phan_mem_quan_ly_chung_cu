b00608c17e31181d6688d0496b0361d3409f9571
ZotAZaw3 <zotazaw3@gmail.com>
2026-06-23 17:43:27 +0700

    Great Idea

--- back-end/src/config/defaultUser.js
+++ back-end/src/config/defaultUser.js
@@ -2,7 +2,7 @@ export const defaultUser = {
   Username: "admin1234",
   Password: "1234",
   FullName: "Bui Minh Long",
-  Email: "Long.BM235140@sis.hust.edu.vn",
+  Email: "bill.gates@microsoft.com",
   PhoneNumber: "0123456789",
   Role: "Tổ trưởng",
 };
